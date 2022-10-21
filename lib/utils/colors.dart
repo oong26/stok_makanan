00f6576bd6b1bde80dad5c40e1b74ef9c2d63d63
oong26 <mkhalil26122000@gmail.com>
2022-10-21 13:58:57 +0700

    develop : adding assets & conf theme

---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const mPrimaryColor = Color(0XFF236068);
+const mDangerColor = Color(0XFFF30808);
+const mWarningColor = Color(0XFFFFD233);
+const mOrangeColor = Color(0XFFE0742D);
+const mGreyColor = Color(0XFF9F9F9F);
+const mSmokeWhiteColor = Color(0XFFD9D9D9);
---
+++ lib/utils/styles.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var titleTextStyle = GoogleFonts.petrona(
+  color: Colors.white,
+  fontWeight: FontWeight.w800,
+  fontSize: 150,
+);
+
+var sectionTitleTextStyle = GoogleFonts.petrona(
+  color: Colors.white,
+  fontWeight: FontWeight.w700,
+  fontSize: 40,
+);
+
+var sectionBodyTextStyle = GoogleFonts.petrona(
+  color: Colors.white,
+  fontWeight: FontWeight.w400,
+  fontSize: 40,
+);
+
+var formTitleTextStyle = GoogleFonts.plusJakartaSans(
+  color: Colors.black,
+  fontWeight: FontWeight.w700,
+  fontSize: 36,
+);
+
+var inputTextStyle = GoogleFonts.plusJakartaSans(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 25,
+);
+
+var body1TextStyle = GoogleFonts.plusJakartaSans(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 14,
+);
+
+var body2TextStyle = GoogleFonts.plusJakartaSans(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 12,
+);
+
+var buttonTextStyle = GoogleFonts.plusJakartaSans(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 13,
+);
